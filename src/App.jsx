7460ca142eff7d431555f922b1c89ba2b8f4d3ea
Mayank <76253494+mayanksharma-1@users.noreply.github.com>
2025-04-26 23:46:53 +0000

    setup the react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import  Navigation  from './components/Navigation'
+import MainBody from './components/MainBody';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Navigation />
+    <MainBody />
+    </>
+  )
+}
+
+export default App;
