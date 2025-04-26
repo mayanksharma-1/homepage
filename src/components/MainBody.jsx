53e5d09123edb2d5768cfdcb9122b75e57a515b5
Mayank <76253494+mayanksharma-1@users.noreply.github.com>
2025-04-26 23:52:08 +0000

    make and style mainbody boilerplate

---
+++ src/components/MainBody.jsx
@@ -0,0 +1,12 @@
+import './stylesheets/MainBody.css';
+function MainBody() {
+  return (
+    <main>
+      <article></article>
+      <article></article>
+      <article></article>
+      <article></article>
+    </main>
+  );
+}
+export default MainBody;
