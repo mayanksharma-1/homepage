6438cbecba6948717da02207cbe2b210caf7d026
Mayank <76253494+mayanksharma-1@users.noreply.github.com>
2025-04-27 23:08:42 +0000

    Add Data.jsx

---
+++ src/components/Data.jsx
@@ -0,0 +1,19 @@
+const data = [
+  {
+    image: 'https://via.placeholder.com/150',
+    title: 'Title 1',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    image: 'https://via.placeholder.com/150',
+    title: 'Title 2',
+    text: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    image: 'https://via.placeholder.com/150',
+    title: 'Title 3',
+    text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+];
+
+export default data;
