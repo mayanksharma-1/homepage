27ca69a34ec9992133fea6da1dd3318b184cf6a2
Mayank <76253494+mayanksharma-1@users.noreply.github.com>
2025-04-28 02:35:41 +0000

    Add project details to Data.js

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,18 +1,26 @@
 const data = [
   {
-    image: 'https://via.placeholder.com/150',
-    title: 'Title 1',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: 'https://i.gyazo.com/384bf8f80cfa1e190ea3a23504d844e5.png',
+    title: 'Better Clipboard: Enhancing Chrome\'s Clipboard Functionality',
+    text: `-Designed and implemented a Chrome extension using JavaScript, HTML, and CSS to enhance
+clipboard management. \n
+- Key features include capturing user-selected text, storing the three most recent snippets using
+chrome.storage, and displaying them in a popup. \n 
+- The extension leverages Chrome Extension APIs (chrome.storage, chrome.tabs, chrome.runtime,
+chrome.scripting) and adheres to Manifest V3 guidelines by avoiding direct system clipboard
+interaction.`,
   },
   {
-    image: 'https://via.placeholder.com/150',
-    title: 'Title 2',
-    text: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: 'https://i.gyazo.com/031e6fb05cbbdc4c5fe0aa8c1c7fa249.png',
+    title: 'ShopAPI',
+    text: `Built a backend API adhering to RESTful principles using Node.js, Express.js, and MongoDB. \n
+- The API provides comprehensive CRUD operations for efficient management of product \n
+information, demonstrating skills in API design, server-side development, and database interactions.`,
   },
   {
-    image: 'https://via.placeholder.com/150',
-    title: 'Title 3',
-    text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: 'https://i.gyazo.com/bd88680cc2292a6ececf3ecbb079580c.png',
+    title: 'etch-a-sketch',
+    text: `A digital Etch-a-Sketch built with HTML, CSS, and JavaScript. Users can draw on a grid-based canvas, simulating the traditional Etch-a-Sketch experience. Key aspects of this project include dynamic manipulation of the Document Object Model (DOM) to update the canvas, handling user events for drawing, and applying CSS for styling. \n`,
   },
 ];
 
