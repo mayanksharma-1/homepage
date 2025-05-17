4c2a8e23e006f8f409de20d5cd894bd6eda47e34
Mayank <76253494+mayanksharma-1@users.noreply.github.com>
2025-05-17 19:24:38 +0000

    Add carousel component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import './App.css'
 import data from './components/Data'; 
-// import Carousel from './components/Carousel';
+import Carousel from './components/Carousel';
 
 function App() {
 
   return (
     <div className="app-container">
       <NavBar />
-      {/* <Carousel articles={data} /> */}
+      <Carousel articles={data} />
     </div>
   );
 }
