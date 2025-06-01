856eeebe269a7fa450acd6affa489d3ff714812c
mayank <as.mayanksharma@gmail.com>
2025-06-01 16:09:15 +0530

    docs: Reformat project summaries for improved readability

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -2,27 +2,27 @@ const data = [
   {
     image: 'https://i.gyazo.com/bd2c3be7fd2c655306b698dcc52222e0.png',
     title: 'Better Clipboard',
-    text: `-Designed and implemented a Chrome extension using JavaScript, HTML, and CSS to enhance
-clipboard management. \n
-- Key features include capturing user-selected text, storing the three most recent snippets using
-chrome.storage, and displaying them in a popup. \n 
-- The extension leverages Chrome Extension APIs (chrome.storage, chrome.tabs, chrome.runtime,
-chrome.scripting) and adheres to Manifest V3 guidelines by avoiding direct system clipboard
-interaction.`,
+    text: `Designed and implemented a Chrome extension using JavaScript, HTML,
+     and CSS to enhance clipboard management. This Manifest V3-compliant tool captures user-selected text,
+     storing the three most recent snippets via chrome.storage and displaying them in a popup. 
+     It effectively leverages Chrome Extension APIs like chrome.tabs, chrome.runtime,
+     and chrome.scripting, all while avoiding direct system clipboard interaction for improved security.`,
   },
   {
     image: 'https://i.gyazo.com/031e6fb05cbbdc4c5fe0aa8c1c7fa249.png',
     title: 'Shop-API',
-    text: `-Built a backend API adhering to RESTful principles using Node.js, Express.js, and MongoDB. \n
--The API provides comprehensive CRUD operations for efficient management of product \n
--information, demonstrating skills in API design, server-side development, and database interactions.`,
+    text: `Built a backend API adhering to RESTful principles using Node.js,
+      Express.js, and MongoDB, providing comprehensive CRUD operations for efficient product information management.
+      This project demonstrates strong skills in API design, server-side development, and database interactions.
+
+`,
   },
   {
     image: 'https://i.gyazo.com/bd88680cc2292a6ececf3ecbb079580c.png',
     title: 'etch-a-sketch',
-    text: `-Developed a digital Etch-a-Sketch application using HTML, CSS, and JavaScript. \n
--Implemented a grid-based canvas allowing users to draw, simulating the classic toy experience. \n
--Utilized dynamic DOM manipulation for canvas updates and handled user events for drawing. \n` ,
+    text: `Developed a digital Etch-a-Sketch application using HTML, CSS, and JavaScript.
+     This project features a grid-based canvas that simulates the classic toy,
+     allowing users to draw by leveraging dynamic DOM manipulation for canvas updates and handling various user events for drawing interactions.` ,
   },
 ];
 
